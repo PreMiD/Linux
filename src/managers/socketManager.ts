7b59e3a8eccb31081a37c5a2635b9830160b7df9
ririxidev <mail@ririxi.dev>
2021-12-18 21:51:03 +0100

    chore: Adapt 2.2.0 changes from @PreMiD/PreMiD

--- src/managers/socketManager.ts
+++ src/managers/socketManager.ts
@@ -1,15 +1,11 @@
-import socketIo from "socket.io";
-import { createServer, Server } from "http";
 import { app, dialog } from "electron";
-import { update as updateSettings } from "./settingsManager";
+import { createServer, Server } from "http";
+import socketIo from "socket.io";
+
+import { trayManager } from "../";
+import { clearActivity, getDiscordUser, rpcClients, setActivity } from "./discordManager";
 import { openFileDialog } from "./presenceDevManager";
-import {
-	rpcClients,
-	setActivity,
-	clearActivity,
-	getDiscordUser
-} from "./discordManager";
-import { trayManager } from "..";
+import { update as updateSettings } from "./settingsManager";
 
 export let io: socketIo.Server;
 export let socket: socketIo.Socket;
@@ -17,17 +13,20 @@ export let server: Server;
 export let connected: boolean = false;
 
 export function init() {
-	return new Promise(resolve => {
+	return new Promise<void>(resolve => {
 		//* Create server
 		//* create SocketIo server, don't server client
 		//* Try to listen to port 3020
 		//* If that fails/some other error happens run socketError
 		//* If someone connects to socket socketConnection
 		server = createServer();
-		io = socketIo(server, { serveClient: false });
-		io.origins((o, c) => {
-			if (o !== "*") c("Not a chrome extension, socket denied.", false);
-			c(null, true);
+		io = new socketIo.Server(server, {
+			serveClient: false,
+			allowEIO3: true,
+			allowRequest: (req, callback) => {
+				const noOriginHeader = req.headers.origin === undefined;
+				callback(null, noOriginHeader);
+			}
 		});
 		server.listen(3020, () => {
 			//* Resolve promise
